c2ca4ee52a4d6dc9ecc7fe20a1d112d9a0557986
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2020-10-03 21:28:41 +0500

    Delete temporary database

--- core/settings.py
+++ core/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "temporary.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
